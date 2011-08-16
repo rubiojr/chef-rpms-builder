fefaf55dd14c942225f87ffc13deffb31ab16f3f
Sergio Rubio <rubiojr@frameos.org>
2011-08-16 15:33:50 +0200

    * updated test for RHEL6

--- test/test-el6.sh
+++ test/test-el6.sh
@@ -14,7 +14,10 @@ run(){
 	fi
 }
 
+run rpm --force -Uvh http://rbel.co/epel6
 run rpm --force -Uvh http://rbel.co/rbel6
+run yum install -y git gcc make gcc-c++ ruby-devel libxml2-devel libxslt-devel
+run rpm -e epel-release
 cat > /etc/yum.repos.d/nightly.repo <<EOF
 [rbel-nightly]
 name=rbel nightly
@@ -22,7 +25,7 @@ baseurl=http://rbel.frameos.org/nightly/el6/\$basearch/
 gpgcheck=1
 enabled=1
 EOF
-run yum install -y rubygem-chef-server
+run yum install -y rubygem-chef-server 
 run setup-chef-server.sh
 run mkdir /root/.chef
 run yum install -y wget
@@ -34,5 +37,8 @@ run tar xzf yum.tgz
 run tar xzf yumrepo.tgz
 run knife cookbook -c /root/.chef/knife.rb upload -o . yum yumrepo
 run knife node -c /root/.chef/knife.rb run_list add vagrant 'recipe[yumrepo]'
+run gem install --no-ri --no-rdoc knife-playground
+run knife pg -c /root/.chef/knife.rb git cookbook upload git://github.com/rubiojr/yum-stress-cookbook.git
+run knife node -c /root/.chef/knife.rb run_list add vagrant 'recipe[yum-stress-cookbook]'
 run rpm --force -Uvh http://rbel.co/epel6
 run chef-client -c /root/.chef/knife.rb
